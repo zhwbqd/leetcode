85c55b3c61b3b2521be65539ec01018c0624a961
jack.zhang <zhwbqd@gmail.com>
2015-07-20 21:33:11 +0800

    add invert binary tree

---
+++ src/main/java/zhwb/study/algorithms/reverse/InvertBinaryTree.java
@@ -0,0 +1,71 @@
+package zhwb.study.algorithms.reverse;
+
+/**
+ * https://leetcode.com/problems/invert-binary-tree/
+ * <p/>
+ * Invert a binary tree.
+ * <p/>
+ * 4
+ * /   \
+ * 2     7
+ * / \   / \
+ * 1   3 6   9
+ * to
+ * 4
+ * /   \
+ * 7     2
+ * / \   / \
+ * 9   6 3   1
+ *
+ * @author jack.zhang
+ * @since 2015/7/20 0020
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return root;
+        } else {
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            invertTree(root.left);
+            invertTree(root.right);
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode origin = new TreeNode(4);
+        TreeNode left = new TreeNode(2);
+        left.left = new TreeNode(1);
+        left.right = new TreeNode(3);
+        TreeNode right = new TreeNode(7);
+        right.left = new TreeNode(6);
+        right.right = new TreeNode(9);
+        origin.left = left;
+        origin.right = right;
+
+        TreeNode after = new InvertBinaryTree().invertTree(origin);
+        System.out.println(after);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("TreeNode{");
+        sb.append("val=").append(val);
+        sb.append(", left=").append(left);
+        sb.append(", right=").append(right);
+        sb.append('}');
+        return sb.toString();
+    }
+}
