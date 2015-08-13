5fd13470b0b40033b61ccbe37f33699a4835879b
jack.zhang <zhwbqd@gmail.com>
2015-08-14 00:00:11 +0800

    three sum

--- src/main/java/zhwb/study/algorithms/array/ThreeSum.java
+++ src/main/java/zhwb/study/algorithms/array/ThreeSum.java
@@ -43,4 +43,8 @@ public class ThreeSum {
         return res;
     }
 
+    public static void main(String[] args) {
+        System.out.println(new ThreeSum().threeSum(new int[]{-1, 0, 1, -2, -1, 4, -3}));
+    }
+
 }
---
+++ src/main/java/zhwb/study/algorithms/array/sub/ThreeSumClosest.java
@@ -0,0 +1,11 @@
+package zhwb.study.algorithms.array.sub;
+
+/**
+ * @author jack.zhang
+ * @since 2015/8/13 0013
+ */
+public class ThreeSumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        return 1;
+    }
+}
