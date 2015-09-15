47d08dbbcacbc60f943cb55754b420386f2368df
jack.zhang <zhwbqd@gmail.com>
2015-09-15 23:23:04 +0800

    add simple solution

--- src/main/java/zhwb/study/algorithms/array/MaximumSubarray.java
+++ src/main/java/zhwb/study/algorithms/array/MaximumSubarray.java
@@ -35,9 +35,18 @@ public class MaximumSubarray {
         return max;
     }
 
+    public int maxSubArrayV2(int[] A) {
+        int max = A[0], curMax = A[0];
+        for (int i = 1; i < A.length; i++) {
+            curMax = Math.max(curMax, 0) + A[i];
+            max = Math.max(max, curMax);
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new MaximumSubarray().maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
-        System.out.println(new MaximumSubarray().maxSubArray(new int[]{-2, -1}));
-        System.out.println(new MaximumSubarray().maxSubArray(new int[]{-1, 0}));
+        System.out.println(new MaximumSubarray().maxSubArrayV2(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println(new MaximumSubarray().maxSubArrayV2(new int[]{-2, -1}));
+        System.out.println(new MaximumSubarray().maxSubArrayV2(new int[]{-1, 0}));
     }
 }
